94c8100375741d683bf6f4828eb6a3c20afa2b8a
Allison Guthrie <allisonguthrie5580@gmail.com>
2018-12-05 20:01:58 -0700

    update serviceworker

--- sw.js
+++ sw.js
@@ -5,23 +5,23 @@ self.addEventListener('install', function(event) {
             return cache.addAll(
                 [
                     '/',
-                    'js/main.js',
-                    'css/styles.css',
-                    'img/1.jpg',
-                    'img/2.jpg',
-                    'img/3.jpg',
-                    'img/4.jpg',
-                    'img/5.jpg',
-                    'img/6.jpg',
-                    'img/7.jpg',
-                    'img/8.jpg',
-                    'img/9.jpg',
-                    'img/10.jpg',
-                    'data/restaurants.json',
-                    'js/dbhelper.js',
-                    'js/restaurant_info.js',
-                    'index.html',
-                    'restaurant.html'
+                    '/js/main.js',
+                    '/css/styles.css',
+                    '/img/1.jpg',
+                    '/img/2.jpg',
+                    '/img/3.jpg',
+                    '/img/4.jpg',
+                    '/img/5.jpg',
+                    '/img/6.jpg',
+                    '/img/7.jpg',
+                    '/img/8.jpg',
+                    '/img/9.jpg',
+                    '/img/10.jpg',
+                    '/data/restaurants.json',
+                    '/js/dbhelper.js',
+                    '/js/restaurant_info.js',
+                    '/index.html',
+                    '/restaurant.html'
                 ]
             );
         })
